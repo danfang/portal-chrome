a6ea4dd484fb4cf361b104447222e531fd47f941
Daniel Fang <danfang@uw.edu>
2016-02-01 06:06:23 -0800

    Removing files from app

--- app/main.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    "id": "identitywin",
-    "innerBounds": {
-      "width": 450,
-      "height": 540
-    }
-  });
-});
